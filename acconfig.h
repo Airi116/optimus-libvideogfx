e603ae8199be58df44293b418d674099e2db3636
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-04 07:31:36 +0000

    fix picture

---
+++ acconfig.h
@@ -0,0 +1,17 @@
+
+@BOTTOM@
+
+#undef HAVE_X11
+
+#undef HAVE_XV
+
+#undef HAVE_LIBJPEG
+
+#undef HAVE_LIBPNG
+
+/* Define if MMX assembly code should be compiled. */
+#undef ENABLE_MMX
+
+#undef CPU_x86
+
+#undef LINUX
---
+++ autogen.sh
@@ -0,0 +1,23 @@
+
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+PROJECT=libvideogfx
+
+if [ -x "`which autoreconf 2>/dev/null`" ] ; then
+   exec autoreconf -ivf
+fi
+
+LIBTOOLIZE=libtoolize
+SYSNAME=`uname`
+if [ "x$SYSNAME" = "xDarwin" ] ; then
+  LIBTOOLIZE=glibtoolize
+fi
+aclocal -I m4 && \
+        autoheader && \
+        $LIBTOOLIZE && \
+        autoconf && \
+        automake --add-missing --force-missing --copy
+
+echo 
+echo "Now type 'make' to compile $PROJECT."
