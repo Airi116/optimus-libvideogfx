bfb7ac97f30a382fb29dfa40bd26ac205d9124e8
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-04 14:59:00 +0000

    playing with pygame

---
+++ examples/audioout.cc
@@ -0,0 +1,56 @@
+
+
+#include <math.h>
+#include <iostream>
+#include "../libvideogfx/libvideogfx.hh"
+#include <unistd.h>
+
+using namespace std;
+using namespace videogfx;
+
+
+int main()
+{
+  try
+    {
+      int16 sine [44100];
+      int16 sine2[44100];
+
+      for (int i=0;i<44100;i++)
+	{
+	  sine [i] = (int)(20000*sin(2*M_PI*i* 500/44100));
+	  sine2[i] = (int)(20000*sin(2*M_PI*i* 750/44100));
+	}
+
+      AudioSink_LinuxSndCard snd;
+      AudioParam param;
+      param.n_channels = 2;
+      param.rate = 44100;
+      snd.SetParam(param);
+      param = snd.AskParam();
+
+      int16 buf[2*44100];
+      for (int i=0;i<44100;i++)
+	{
+	  buf[2*i  ]=sine [i];
+	  buf[2*i+1]=sine2[i];
+	}
+
+      for (int i=0;i<5;i++)
+	{
+	  snd.SendSamples(buf,2*44100);
+	}
+
+      while (snd.PresentationDataPending())
+	{
+	  snd.PresentData(snd.NextDataPresentationTime());
+	  usleep(100000);
+	}
+    }
+  catch (const Excpt_Base& e)
+    {
+      MessageDisplay::Show(e);
+    }
+
+  return 0;
+}
---
+++ examples/bitstream_output.cc
@@ -0,0 +1,31 @@
+
+#include "../libvideogfx/utility/bitstream/bitbuffer.hh"
+#include <iostream>
+
+using namespace std;
+using namespace videogfx;
+
+void ShowBuffer(const uint8* b,int len)
+{
+  for (int i=0;i<len;i++)
+    {
+      if (i>0 && i%8==0) cout << endl;
+      if (i%8 != 0) cout << " ";
+
+      for (int n=0x80;n;n>>=1)
+	if (b[i]&n) cout << '1';
+	else cout << '0';
+    }
+
+  cout << endl;
+}
+
+int main()
+{
+  BitBuffer buf;
+
+  buf.WriteBitsMasked(~0,7);
+  buf.WriteBitsMasked( 0,14);
+  buf.WriteBitsMasked(~0,20);
+
+  buf.WriteBitsMasked
