00b19740f4b5b8504355c8df1cb5705f630c7834
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-04 22:14:28 +0000

    add new logo

--- examples/copytest.cc
+++ examples/copytest.cc
@@ -3,4 +3,55 @@
 #include "../libvideogfx/graphics/fileio/jpeg.hh"
 #include "../libvideogfx/graphics/draw/blit.hh"
 #include "../libvideogfx/graphics/draw/format.hh"
-#include "../libvi
+#include "../libvideogfx/graphics/draw/scale.hh"
+using namespace videogfx;
+
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+// ARGS: give me 4 JPEG files
+
+int main(int argc,char** argv)
+{
+  srand(time(NULL));
+
+  Image<Pixel> img[5];
+  for (int i=0;i<4;i++)
+    {
+      ReadImage_JPEG(img[i], argv[i+1]);
+    }
+
+  ImageParam spec = img[0].AskParam();
+  img[4].Create(spec);
+
+  ImageWindow_Autorefresh_X11 win;
+  win.Create(spec.width,spec.height,"test");
+
+  int w = spec.width;
+  int h = spec.height;
+
+#if 0
+  Copy(img[4],0,0,
+       img[0],0,0,w/2,h/2);
+  Copy(img[4],w/2,0,
+       img[1],w/2,0,w/2,h/2);
+  Copy(img[4],0,h/2,
+       img[2],0,h/2,w/2,h/2);
+  Copy(img[4],w/2,h/2,
+       img[3],w/2,h/2,w/2,h/2);
+#else
+  CopyScaled(img[4],0,0,w/2,h/2,
+	     img[0],0,0,w,h);
+  CopyScaled(img[4],w/2,0,w/2,h/2,
+	     img[1],0,0,w,h);
+  CopyScaled(img[4],0,h/2,w/2,h/2,
+	     img[2],0,0,w,h);
+  CopyScaled(img[4],w/2,h/2,w/2,h/2,
+	     img[3],0,0,w,h);
+#endif
+
+  for (int i=0;i<5;i++)
+    {
+      win.Displ
