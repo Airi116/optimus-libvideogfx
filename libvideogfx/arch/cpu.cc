c407687b87239e28fa9036119b19333cd17aed61
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-07 06:18:50 +0000

    chore(package): update sinon to version 7.2.3

--- libvideogfx/arch/cpu.cc
+++ libvideogfx/arch/cpu.cc
@@ -15,4 +15,44 @@
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *******************************
+ ********************************************************************************/
+
+#include <libvideogfx/arch/cpu.hh>
+#include <libvideogfx/error.hh>
+#include <libvideogfx/types.hh>
+#include "config.h"
+
+#include <string.h>
+#include <fstream>
+
+namespace videogfx {
+  using namespace std;
+
+  const char* CPU_Capabilities::ArchName(CPU_Architecture arch)
+  {
+    switch (arch)
+      {
+      case CPUArch_X86:     return "intel x86"; break;
+      case CPUArch_68k:     return "680x0"; break;
+      case CPUArch_PowerPC: return "PowerPC"; break;
+      case CPUArch_ARM:     return "ARM"; break;
+      case CPUArch_Sparc:   return "Sparc"; break;
+      case CPUArch_HPPA:    return "HP-PA"; break;
+      default:              return "unknown"; break;
+      }
+  }
+
+
+
+  class CPU_Generic : public CPU_Capabilities
+  {
+    // nothing special
+  };
+
+  // --------------------------------------------------------------------------
+
+#if CPU_x86
+
+#define CPU_CAP_MMX      (1<<0)
+#define CPU_CAP_MMXEXT   (1<<1)
+#define CPU_CAP
