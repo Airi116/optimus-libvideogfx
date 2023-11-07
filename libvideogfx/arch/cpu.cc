e9b80e2d875c3fcbc4f310c46e29ddd52ea7198e
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-07 13:27:49 +0000

    added another SCP

--- libvideogfx/arch/cpu.cc
+++ libvideogfx/arch/cpu.cc
@@ -55,4 +55,45 @@ namespace videogfx {
 
 #define CPU_CAP_MMX      (1<<0)
 #define CPU_CAP_MMXEXT   (1<<1)
-#define CPU_CAP
+#define CPU_CAP_SSE      (1<<2)
+#define CPU_CAP_SSE2     (1<<3)
+#define CPU_CAP_SSE3     (1<<4)
+#define CPU_CAP_SSSE3    (1<<5)
+#define CPU_CAP_SSE4_1   (1<<6)
+#define CPU_CAP_SSE4_2   (1<<7)
+#define CPU_CAP_AVX      (1<<8)
+#define CPU_CAP_SSE4a    (1<<9)
+#define CPU_CAP_3Dnow    (1<<10)
+#define CPU_CAP_3DnowExt (1<<11)
+#define CPU_CAP_CMOV     (1<<12)
+
+  class CPU_X86 : public CPU_Capabilities
+  {
+  public:
+    CPU_X86()
+    {
+      m_flags = 0;
+
+      uint32_t regs[4];
+
+      int a = 1;
+
+#ifdef _MSC_VER
+      __cpuid((int *)regs, (int)a);
+
+#else
+      __asm__ volatile
+	("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
+	 : "a" (a), "c" (0));
+      // ECX is set to zero for CPUID function 4
+#endif
+  
+      // printf("CPUID EAX=1 -> ECX=%x EDX=%x\n", regs[2], regs[3]);
+
+      uint32_t ecx = regs[2];
+      uint32_t edx = regs[3];
+
+      if ((edx & (1<<15))) m_flags |= CPU_CAP_CMOV;
+      if ((edx & (1<<23))) m_flags |= CPU_CAP_MMX;
+      if ((edx & (1<<25))) m_flags |= CPU_CAP_SSE;
+      if ((edx 
