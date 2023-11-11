9f05ea1817bc78ca6d0f038f3ef76a0b43dff023
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-11 17:02:21 +0000

    Deploy reduxframework/docs to github.com/reduxframework/docs.git:gh-pages

--- libvideogfx/audio/fileio/audiosink.cc
+++ libvideogfx/audio/fileio/audiosink.cc
@@ -10,4 +10,45 @@ namespace videogfx {
 
     while (len)
       {
-	int 
+	int l = std::min(len,1000);
+	for (int i=0;i<l;i++)
+	  buf[i] = samples[i] << 8;
+
+	SendSamples(buf,l);
+	len -= l;
+      }
+  }
+
+  void AudioSink::SendSamples(const int16* samples,int len)
+  {
+    // MessageDisplay::Show(ErrSev_Warning,"No 16 bit audio output defined. Most probably, this is an error.");
+
+    int32 buf[1000];
+
+    while (len)
+      {
+	int l = std::min(len,1000);
+	for (int i=0;i<l;i++)
+	  buf[i] = samples[i] << 16;
+
+	SendSamples(buf,l);
+	len -= l;
+      }
+  }
+
+  void AudioSink::SendSamples(const int32* samples,int len)
+  {
+    int16 buf[1000];
+
+    while (len)
+      {
+	int l = std::min(len,1000);
+	for (int i=0;i<l;i++)
+	  buf[i] = samples[i] >> 16;
+
+	SendSamples(buf,l);
+	len -= l;
+      }
+  }
+
+}
---
+++ libvideogfx/audio/fileio/audiosink.hh
@@ -0,0 +1,16 @@
+/********************************************************************************
+  audio/fileio/audiosink.hh
+    Generic audio data sink.
+
+  purpose:
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+   14/Apr/2002 - Dirk Farin
+     - new inter
