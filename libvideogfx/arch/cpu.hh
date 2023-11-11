d3a88a78a737b2cf64aa2dff9659aa8443411965
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-11 08:58:46 +0000

    Add new test cases

--- libvideogfx/arch/cpu.hh
+++ libvideogfx/arch/cpu.hh
@@ -25,4 +25,37 @@
 
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#ifndef LIBVIDEOGFX_ARCH_CPU_HH
+#define LIBVIDEOGFX_ARCH_CPU_HH
+
+#include <libvideogfx/error.hh>
+
+namespace videogfx {
+
+  enum CPU_Architecture
+  {
+    CPUArch_X86, CPUArch_68k, CPUArch_PowerPC, CPUArch_ARM, CPUArch_Sparc,
+    CPUArch_HPPA,
+    CPUArch_Unknown
+  };
+
+
+  class CPU_Capabilities
+  {
+  public:
+    static CPU_Capabilities* AskCapabilities(); // get capabilities of CPU
+
+
+    virtual ~CPU_Capabilities() { }
+
+    virtual void GetCPUInfo(char* buf,int maxChars,bool long_description=false);
+    virtual CPU_Architecture AskArchitecture() const { return CPUArch_Unknown; }
+    const char* GetArchitectureName() const
