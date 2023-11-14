3f8e5989414b2666a0210af6787ad4a6b210be4e
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-14 17:51:54 +0000

    Minor formatting updates

--- libvideogfx/audio/fileio/audiosink_linux.hh
+++ libvideogfx/audio/fileio/audiosink_linux.hh
@@ -6,4 +6,24 @@
     LibVideoGfx - video processing library
     Copyright (C) 2002  Dirk Farin
 
-    This library is free software; yo
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#ifndef LIBVIDEOGFX_AUDIO_FILEIO_AUDIOSINK_LINUX_HH
+#define LIBVIDEOGFX_AUDIO_FILEIO_AUDIOSINK_LINUX_HH
+
+#include <libvideogfx/audio/fileio/audiosink.hh>
+#include <libvideogfx/audio/fileio/timedsink.hh>
+#include <libvideogfx/utility/bitstream/bytebuff
