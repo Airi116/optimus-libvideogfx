2bdee112406d6fa259d07accfacbb305740ba3e3
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-15 22:44:04 +0000

    adding copyright

---
+++ libvideogfx/containers/array.hh
@@ -0,0 +1,29 @@
+/********************************************************************************
+  libvideogfx/containers/array.hh
+
+  purpose:
+    Simple 1dim array class template.
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+   19/Sep/2000 - Dirk Farin
+   - New method CreateRange()
+   30/Jun/2000 - Dirk Farin
+   - first implementation
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2002  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warra
