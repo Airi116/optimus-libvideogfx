ef4f142dd86c53112ab3a419e14c14bf178b378a
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-28 13:41:21 +0000

    fix structure

---
+++ libvideogfx/containers/dynarray.hh
@@ -0,0 +1,21 @@
+/********************************************************************************
+  libvideogfx/containers/dynarray.hh
+
+  purpose:
+    Very simple dynamic array class.
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+   29/Aug/2001 - Dirk Farin
+   - first implementation
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2002  Dirk Farin
+
+  
