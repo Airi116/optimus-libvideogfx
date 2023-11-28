3c9d6b743e6027c8513698765578dff8e55b2cb9
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-28 22:19:47 +0000

    pagination tests

--- libvideogfx/containers/dynarray.hh
+++ libvideogfx/containers/dynarray.hh
@@ -39,4 +39,47 @@
 #include "libvideogfx/types.hh"
 #include <assert.h>
 
-namespace vide
+namespace videogfx {
+
+  template <class T> class DynArray
+  {
+  public:
+    DynArray();
+    DynArray(int initial_size);
+    DynArray(const DynArray<T>&);
+    ~DynArray();
+
+    void Clear();
+    void Append(const T& t);
+
+    /* Append a new, empty element and return a reference to it. */
+    T&   AppendNewEntry() { T t; Append(t); return d_array[d_nentries-1]; }
+
+    /* Insert the element at the specified position. */
+    void Insert(int pos,const T& t);
+    T    ReturnAndRemoveEntry(int n);
+    void RemoveEntry(int n);
+    int  AskSize() const { return d_nentries; }
+
+    void SetEmptyValue(const T& e) { d_empty_value=e; d_empty_val_set=true; }
+
+    const T& operator[](int n) const { return d_array[n]; }
+    T& operator[](int n)
+    {
+      EnlargeToSize(n+1);
+      if (n>=d_nentries)
+	d_nentries = n+1;
+      return d_array[n];
+    }
+
+    const DynArray<T> operator=(const DynArray<T>&);
+
+  private:
+    T    d_empty_value;
+    bool d_empty_val_set;
+
+    T*   d_array;
+    int  d_nentries;
+    int  d_size;
+
+    void Enla
