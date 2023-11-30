964f882ff5b443e225011dfe59f66cf25b30f6e5
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-30 10:08:55 +0000

    Fix project name.

---
+++ libvideogfx/containers/queue.hh
@@ -0,0 +1,20 @@
+/********************************************************************************
+  utility/queue.hh
+
+  purpose:
+    Queue container class template.
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+   29/Sep/1999 - Dirk Farin
+     - integrated into libvideogfx CVS
+   20/Nov/1998 - Dirk Farin
+     - new method: AskAndRemoveHead()
+ ********************************************************************************
+    LibVideoGfx - video p
