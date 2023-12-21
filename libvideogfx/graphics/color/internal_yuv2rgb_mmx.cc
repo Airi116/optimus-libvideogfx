d10af3cf5329ebbb260546e01faf0789bfbd1a23
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-12-21 07:32:33 +0000

    sched: Queue RT tasks to head when prio drops

--- libvideogfx/graphics/color/internal_yuv2rgb_mmx.cc
+++ libvideogfx/graphics/color/internal_yuv2rgb_mmx.cc
@@ -31,4 +31,29 @@ using namespace std;
 uint64 UVoffset   = 0x0080008000800080LL;     //   0  4x  128   // UV offs
 uint64 Yoffset    = 0x1010101010101010LL;     //   8  8x   16   // Y offs
 uint64 Cb2Rfact   = 0x0066006600660066LL;     //  16  4x  102 =  409/4         // Cb  ->R
-uint64 CbCr2Gfact = 0x0034001900340019LL;     //  24  2x (52 25) = 208/4 100/4 //
+uint64 CbCr2Gfact = 0x0034001900340019LL;     //  24  2x (52 25) = 208/4 100/4 // CbCr->G
+uint64 Cb2Bfact   = 0x0081008100810081LL;     //  32  4x  129 =  516/4         // Cb  ->B
+uint64 Yfact      = 0x004A004A004A004ALL;     //  40  4x   74 =  298/4         // Y mul
+uint64 shift6bit  = 0x0000000000000006LL;     //  40  4x   74 =  298/4         // Y mul
+
+uint64 tmp_cr, tmp_rimpact, tmp_rimpact2, tmp_gimpact, tmp_bimpact, tmp_bimpact2;
+
+
+
+namespace videogfx {
+
+  bool i2r_16bit_mmx::s_CanConvert(const Image<Pixel>& img,const RawRGBImageSpec& spec)
+  {
+    if (spec.dest_width || spec.upscale_factor || spec.downscale_factor) return false;
+    if (spec.bits_per_pixel != 16) return false;
+    if (!spec.little_endian) return false;
+
+    ImageParam param = img.AskParam();
+
+    if (param.colorspace != Colorspace_YUV) return false;
+    if (param.chroma != Chroma_420) return false;
+
+    if ((param.width & 7) != 0) return false;
+
+    int w = (param.width+7) & ~7;
+    if (spec.bytes_per_line < 2*w) return false;
