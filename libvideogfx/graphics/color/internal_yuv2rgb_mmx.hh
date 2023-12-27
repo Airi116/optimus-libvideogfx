41da68b9cd6b4acb3645549d49823859dceb7e6d
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-12-27 13:11:16 +0000

    Removed .idea

--- libvideogfx/graphics/color/internal_yuv2rgb_mmx.hh
+++ libvideogfx/graphics/color/internal_yuv2rgb_mmx.hh
@@ -32,4 +32,34 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ********************************************************************************/
 
-#ifndef LIBVIDEOGFX_G
+#ifndef LIBVIDEOGFX_GRAPHICS_COLOR_INTERNAL_YUV2RGB_MMX_HH
+#define LIBVIDEOGFX_GRAPHICS_COLOR_INTERNAL_YUV2RGB_MMX_HH
+
+#include <libvideogfx/graphics/color/img2raw.hh>
+#include "libvideogfx/graphics/color/internal_img2raw.hh"
+
+namespace videogfx {
+
+  class i2r_16bit_mmx : public Image2RawRGB_Transform
+  {
+  public:
+    virtual ~i2r_16bit_mmx() { }
+
+    virtual void Transform(const Image<Pixel>&,uint8* mem,int firstline,int lastline);
+
+    static bool s_CanConvert(const Image<Pixel>&,const RawRGBImageSpec&);
+    virtual bool CanConvert(const Image<Pixel>& i,const RawRGBImageSpec& s) { return s_CanConvert(i,s); }
+
+    virtual const char* TransformationName() { return "YUV to 16bit RGB, MMX accelerated"; }
+  };
+
+
+  class i2r_32bit_BGR_mmx : public Image2RawRGB_Transform
+  {
+  public:
+    virtual ~i2r_32bit_BGR_mmx() { }
+
+    virtual void Transform(const Image<Pixel>&,uint8* mem,int firstline,int lastline);
+
+    static bool s_CanConvert(const Image<Pixel>&,const RawRGBImageSpec&);
+    virtual bool CanConve
