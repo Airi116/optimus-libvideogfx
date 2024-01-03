192fc395696b4b5dd8beaaae50735ecf016ed5a2
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-01-03 15:43:27 +0000

    screen shot

--- libvideogfx/graphics/datatypes/bitmap.hh
+++ libvideogfx/graphics/datatypes/bitmap.hh
@@ -9,4 +9,22 @@
 
   modifications:
     23/Jan/2002 - Dirk Farin - Complete reimplementation based on old Bitmap type.
-    02/Jun/1999 - Dirk Farin -
+    02/Jun/1999 - Dirk Farin - First implementation based on DVDview code.
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2002  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **************************
