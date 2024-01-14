24f8a44f3d5ead816d4b6dfa7e77118b41dacedf
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-01-14 10:31:26 +0000

    xen/pcifront: Fix mysterious crashes when NUMA locality information was extracted.

---
+++ libvideogfx/graphics/datatypes/image.hh
@@ -0,0 +1,18 @@
+/********************************************************************************
+  $Header$
+
+  purpose:
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+    24/Jan/2002 - Dirk Farin - Complete reimplementation based on old Image type.
+    02/Jun/1999 - Dirk Farin - first implementation
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Co
