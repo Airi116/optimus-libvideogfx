b32250019c7eeaf9d3095bb051fbf6aeec3fe6cc
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-01-20 08:00:35 +0000

    file rename

--- libvideogfx/graphics/draw/blit.hh
+++ libvideogfx/graphics/draw/blit.hh
@@ -25,4 +25,25 @@
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser Ge
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#ifndef LIBVIDEOGFX_GRAPHICS_DRAW_BLIT_HH
+#define LIBVIDEOGFX_GRAPHICS_DRAW_BLIT_HH
+
+#include <libvideogfx/graphics/datatypes/bitmap.hh>
+#include <libvideogfx/graphics/datatypes/image.hh>
+#include <libvideogfx/graphics/datatypes/primitives.hh>
+
+namespace videogfx {
+
+  /* Copy bitmap "src" into "dst". The destination bitmap will be created to have the
+     same size as the source bitmap. If the source bitmap is empty, then the destination
+     bitmap content is released.
+     If the destination is shared, a new bitmap will be created.
+  */
+  template <class Pel> void CopyToNew(Bitmap<Pel>& dst,const B
