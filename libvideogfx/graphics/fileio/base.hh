368e98254c7d7020d3d3f7e3b9764bde90a4236f
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-11 09:24:47 +0000

    Auto publish

---
+++ libvideogfx/graphics/fileio/base.hh
@@ -0,0 +1,17 @@
+/*********************************************************************
+  libvideogfx/graphics/fileio/base.hh
+
+  purpose:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2002  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU 
