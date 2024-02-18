ce1b78d732b973ad01e80e4c35dc70e124a31097
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-18 09:05:37 +0000

    Update cats-problem

--- libvideogfx/graphics/fileio/mpeg.cc
+++ libvideogfx/graphics/fileio/mpeg.cc
@@ -10,4 +10,48 @@
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public Li
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#include "libvideogfx/graphics/fileio/mpeg.hh"
+#include <stdlib.h>
+#include <algorithm>
+
+namespace videogfx {
+  using namespace std;
+
+  FileReader_MPEG::FileReader_MPEG()
+    : d_fh(NULL),
+      d_next_framenr(0),
+      d_image_cache_full(false)
+  {
+  }
+
+  FileReader_MPEG::~FileReader_MPEG()
+  {
+    if (d_fh) pclose(d_fh);
+  }
+
+  void FileReader_MPEG::Open(const char* filename)
+  {
+    if (d_fh) pclose(d_fh);
+
+    char buf[100];
+    sprintf(buf,"dvdview -L -W - %s",filename);
+#ifdef _WIN32
+    d_fh = popen(buf,"rb");
+#else
+    d_fh = popen(buf,"r");
+#endif
+  }
+
+  bool FileReader_MPEG::IsEOF() const
+  {
+    if (d_image_cache_full)
+      return false;
+
+    //d_image_cache_f
