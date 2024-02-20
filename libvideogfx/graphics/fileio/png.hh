97d97d509685791b93b1c1dcd39fc56ce8abc2e6
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-20 11:39:29 +0000

    tiny refactoring

--- libvideogfx/graphics/fileio/png.hh
+++ libvideogfx/graphics/fileio/png.hh
@@ -26,4 +26,29 @@
     version 2.1 of the License, or (at your option) any later version.
 
     This library is distributed in the hope that it will be useful,
-    but WITHO
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#ifndef LIBVIDEOGFX_GRAPHICS_FILEIO_PNG_HH
+#define LIBVIDEOGFX_GRAPHICS_FILEIO_PNG_HH
+
+#include <libvideogfx/graphics/datatypes/image.hh>
+#include <iostream>
+
+namespace videogfx {
+
+  bool PNG_Supported();
+
+  void ReadImage_PNG(Image<Pixel>& img, std::istream& stream);
+  void WriteImage_PNG(std::ostream& stream,const Image<Pixel>& img);
+  bool ReadImage_PNG(Image<Pixel>& img, const char* filename);
+  void WriteImage_PNG(const char* filename,const Image<Pixel>& img);
+
+  // obsolete
+  inline void WriteImage_PNG(const Image<Pixel>& img, std::ostream& strea
