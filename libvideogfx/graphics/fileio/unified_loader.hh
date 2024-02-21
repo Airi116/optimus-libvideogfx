65511fbbee0850cd3c2172ff8a7d8b58cd4a9de5
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-21 06:05:17 +0000

    Aesthetics.

--- libvideogfx/graphics/fileio/unified_loader.hh
+++ libvideogfx/graphics/fileio/unified_loader.hh
@@ -22,4 +22,25 @@
     cache -- add a temporary disk-based image cache to allow searching
     rgb -- generate alternating R,G,B images
 
-  author(
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+   30/Jul/04 - Dirk Farin - first implementation
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2004  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Tem
