52611765f8fde40f13aaa8b59fc3a5225fccccad
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-25 18:32:11 +0000

    removing old nightly packages

--- libvideogfx/graphics/fileio/v4l.hh
+++ libvideogfx/graphics/fileio/v4l.hh
@@ -2,4 +2,30 @@
   v4l.hh
 
   purpose:
-    Interfac
+    Interface to Video4Linux grabbing interface.
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+   11/Jul/2000 - Dirk Farin - first implementation
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2002  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along 
