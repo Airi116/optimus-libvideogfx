98a18d87c34c0010632aca9b563d2e5b4fc3676a
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-27 06:53:50 +0000

    new structure

--- libvideogfx/graphics/fileio/yuv_vqeg.cc
+++ libvideogfx/graphics/fileio/yuv_vqeg.cc
@@ -15,4 +15,53 @@
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *********************************************************
+ ********************************************************************************/
+
+#include "libvideogfx/graphics/fileio/yuv_vqeg.hh"
+
+namespace videogfx {
+  using namespace std;
+
+  FileReader_YUV_VQEG::FileReader_YUV_VQEG()
+    : d_yuvstr(NULL), d_mode625(false),
+      d_nextframe(0)
+  {
+  }
+
+
+
+  void FileReader_YUV_VQEG::SkipToImage(int nr)
+  {
+    assert(nr>=0);
+    assert(nr<AskNFrames());
+
+    int size;
+    if (d_mode625) size = 829440; else size = 699840;
+
+    d_yuvstr->seekg(nr*size,ios::beg);
+    d_nextframe=nr;
+  }
+
+
+
+  void FileReader_YUV_VQEG::ReadImage(Image<Pixel>& img)
+  {
+    ImageParam spec;
+    spec.width = 720;
+    spec.height = (d_mode625 ? 576 : 486);
+    spec.chroma = Chroma_422;
+    spec.colorspace = Colorspace_YUV;
+
+    img.Create(spec);
+
+    Pixel*const* yp = img.AskFrameY();
+    Pixel*const* up = img.AskFrameU();
+    Pixel*const* vp = img.AskFrameV();
+
+    char line[1440];
+
+    for (int y=0;y<spec.height;y++)
+      {
+	d_yuvstr->read(line,1440);
+
+	// TODO: 525: bottom field fi
