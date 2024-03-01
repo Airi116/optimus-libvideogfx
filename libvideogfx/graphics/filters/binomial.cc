0f188d89cbff4227841a31c42273d8309f915db1
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-01 11:07:53 +0000

    zsmalloc: change return value unit of zs_get_total_size_bytes

--- libvideogfx/graphics/filters/binomial.cc
+++ libvideogfx/graphics/filters/binomial.cc
@@ -371,4 +371,45 @@ namespace videogfx {
 	    psrlw_i2r(8,mm1);       // mm1: I07 I05 I03 I01
 	    pand_m2r(lb,mm2);       // mm2: I06 I04 I02 I00
 
-	    // "deinterla
+	    // "deinterlace" 8 pixels at (x+8) to 16bit to (mm3,mm4)
+
+	    movq_m2r(*sp1_p1,mm3);
+	    movq_r2r(mm3,mm4);
+	    pand_m2r(hb,mm3);
+	    psrlw_i2r(8,mm3);       // mm3: I15 I13 I11 I09
+	    pand_m2r(lb,mm4);       // mm4: I14 I12 I10 I08
+
+	    // --- process first 4 output pixels ---
+
+	    // construct mm5 with: I13 I11 I09 I07
+
+	    movq_r2r(mm3,mm5);
+	    movq_r2r(mm1,mm6);
+	    psllq_i2r(16,mm5);
+	    psrlq_i2r(48,mm6);
+	    por_r2r(mm6,mm5);
+
+	    //  sum filter in mm5:     I13 I11 I09 I07
+
+	    paddw_r2r(mm4,mm5);  // 2* I14 I12 I10 I08
+	    paddw_r2r(mm4,mm5);
+	    paddw_r2r(mm3,mm5);  //    I15 I13 I11 I09
+
+	    paddw_m2r(a2,mm5);
+	    psrlw_i2r(2,mm5); // divide by 4
+
+	    // --- process next 4 output pixels ---
+
+	    // construct mm0 with: I05 I03 I01 I-1
+
+	    movq_m2r(*sp1_m1,mm7);
+	    pand_m2r(hb,mm7);       // mm7: I-1 xxx xxx xxx
+	    psrlq_i2r(56,mm7);
+
+	    movq_r2r(mm1,mm0);
+	    psllq_i2r(16,mm0);
+	    por_r2r(mm7,mm0);
+
+	    // sum filter in mm0:      I05 I03 I01 I-1
+
+	    pad
