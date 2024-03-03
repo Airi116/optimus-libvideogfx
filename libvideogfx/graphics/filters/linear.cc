b66d6ae8811fd6794a88711a16998095805e7732
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-03 18:16:44 +0000

    Update participants.yml

--- libvideogfx/graphics/filters/linear.cc
+++ libvideogfx/graphics/filters/linear.cc
@@ -26,4 +26,40 @@ using namespace std;
 namespace videogfx {
 
 #if 0
-  /* Low-pass filtering is done using this kernel
+  /* Low-pass filtering is done using this kernel:
+
+     1   /  0  1  0  \
+     - * |  1  4  1  |
+     8   \  0  1  0  /
+  */
+  void LowPass_5pt(const Image_YUV<Pixel>& img,Image_YUV<Pixel>& dest);
+
+
+  /* Low-pass filtering is done using this kernel:
+
+     1   /  1  1  1  \
+     - * |  1  1  1  |
+     9   \  1  1  1  /
+  */
+  void LowPass_3x3mean(const Image_YUV<Pixel>& img,Image_YUV<Pixel>& dest);
+
+  /* Apply interpolation filter with the kernel (1 3 3 1)//4 .
+   * 'src' and 'dst' may be the same.
+   */
+  void InterpolateH_Tap4(const Pixel*const* src,Pixel*const* dst,int dst_width,int height);
+  void InterpolateV_Tap4(const Pixel*const* src,Pixel*const* dst,int width,int dst_height);
+#endif
+
+
+#if 0
+  void LowPass_5pt(const Image_YUV<Pixel>& img,Image_YUV<Pixel>& dest)
+  {
+    // Get image parameters and assure that they are in the right format.
+    ImageParam_YUV param;
+    img.GetParam(param);
+
+    ImageParam_YUV param2;
+    dest.GetParam(param2);
+
+    assert(param.chroma ==Chroma444);
+    assert(param2.chroma==Chroma44
