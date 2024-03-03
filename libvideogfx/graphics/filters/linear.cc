a469154418cac6f37d3d1c80dcfc83de2ad2b279
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-03 15:57:21 +0000

    Added delete button

--- libvideogfx/graphics/filters/linear.cc
+++ libvideogfx/graphics/filters/linear.cc
@@ -1,3 +1,29 @@
 /********************************************************************************
     LibVideoGfx - video processing library
-    Copyright (C) 2002  Dir
+    Copyright (C) 2002  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#include "libvideogfx/graphics/filters/linear.hh"
+
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+namespace videogfx {
+
+#if 0
+  /* Low-pass filtering is done using this kernel
