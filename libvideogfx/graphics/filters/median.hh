560df3ff2b2a58875d627e55a73d6ffe8a359c2f
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-09 05:32:38 +0000

    adding comment

--- libvideogfx/graphics/filters/median.hh
+++ libvideogfx/graphics/filters/median.hh
@@ -27,4 +27,24 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should hav
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#ifndef LIBVIDEOGFX_GRAPHICS_FILTERS_MEDIAN_HH
+#define LIBVIDEOGFX_GRAPHICS_FILTERS_MEDIAN_HH
+
+#include <libvideogfx/graphics/datatypes/image.hh>
+#include <libvideogfx/graphics/draw/blit.hh>
+#include <libvideogfx/containers/array.hh>
+
+
+namespace videogfx {
+
+  /* Radius is 1 for 3x3 and 2 for 5x5, ...
+   */
+  template <class Pel> void Median(Bitmap<Pel>& dst,const Bitmap<Pel>& src,int radius=1);
+}
+
+#endif
