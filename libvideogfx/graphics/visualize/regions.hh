dc83a942b4dc661136ca7796156b52afa85190d3
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-19 11:16:01 +0000

    initial project setup

--- libvideogfx/graphics/visualize/regions.hh
+++ libvideogfx/graphics/visualize/regions.hh
@@ -30,4 +30,22 @@
 
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple 
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#ifndef LIBVIDEOGFX_GRAPHICS_VISUALIZE_REGIONS_HH
+#define LIBVIDEOGFX_GRAPHICS_VISUALIZE_REGIONS_HH
+
+#include <libvideogfx/types.hh>
+#include <libvideogfx/graphics/datatypes/image.hh>
+
+namespace videogfx {
+
+  void DrawRegionBoundaries(Bitmap<Pixel>& bm,const Bitmap<int>& regionmap, bool draw_border=true);
+  void OverlayRegionBoundaries(Bitmap<Pixel>& bm,const Bitmap<int>& regionmap, Pixel value);
+  void OverlayAlphaMask(Bitmap<Pixel>& bm,const Bitmap<Pixel>& alphamap, Pixel val, int threshold=128);
+
+}
+
+#endif
+
