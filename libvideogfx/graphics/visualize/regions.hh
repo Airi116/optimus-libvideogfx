2ee1e5c83986452c4f6357c7a98e6e3e107cc9c2
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-19 10:33:55 +0000

    Updated Spanish translations.

--- libvideogfx/graphics/visualize/regions.hh
+++ libvideogfx/graphics/visualize/regions.hh
@@ -9,4 +9,25 @@
   to do:
 
   author(s):
-   - Dirk Farin, dirk.fa
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+    1999/Nov/02 - Dirk Farin - rewritten because original source
+                               was lost :(((
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2002  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple 
