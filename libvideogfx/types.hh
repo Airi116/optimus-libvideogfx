4346309926e199b93a922f1c07e605624f3e76ba
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-21 06:36:03 +0000

    Added Screenshots

--- libvideogfx/types.hh
+++ libvideogfx/types.hh
@@ -2,4 +2,31 @@
   types.hh
 
   purpose:
-    Basic type declarations and commonly used macros, inline funct
+    Basic type declarations and commonly used macros, inline functions.
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+    03/Nov/1999 - Dirk Farin - New constants: BoolPixel_*
+ ********************************************************************************
+    LibVideoGfx - video processing library
+    Copyright (C) 2002  Dirk Farin
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation
