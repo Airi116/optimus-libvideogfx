d9072766e8dec2e1644ef74751aaea866af13f38
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-28 16:46:47 +0000

    Fixed merge conflicts.

--- libvideogfx/utility/fastalloc.hh
+++ libvideogfx/utility/fastalloc.hh
@@ -28,4 +28,39 @@
     version 2.1 of the License, or (at your option) any later version.
 
     This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without ev
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#ifndef LIBVIDEOGFX_UTILITY_FASTALLOC_HH
+#define LIBVIDEOGFX_UTILITY_FASTALLOC_HH
+
+#include <libvideogfx/types.hh>
+
+namespace videogfx {
+
+  class MemoryAllocator
+  {
+  public:
+    MemoryAllocator(int MinimumMemorySize=1000,int PoolSize=10);
+    ~MemoryAllocator();
+
+    void* Alloc(int size,int* realsize=NULL);
+    void  Free(void*);
+
+    void  ResetPool(); // delete all the memory in the pool
+
+  private:
+    int** d_Pool;
+    int     d_nAreasInPool;
+    int     d_PoolSize;
+    int     d_MinMemSize;
+  };
+}
+
+#endif
