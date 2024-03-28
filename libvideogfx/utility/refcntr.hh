df28db4f51f97b9223c394c3fb0c1f14cbac6c88
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-03-28 21:05:42 +0000

    Remove comment.

---
+++ libvideogfx/utility/refcntr.hh
@@ -0,0 +1,12 @@
+/*********************************************************************
+  libvideogfx/utility/refcntr.hh
+
+  purpose:
+    Base class for reference-counted objects.
+
+  notes:
+
+  to do:
+
+  author(s):
+ 
