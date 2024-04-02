04388d2116f2bc2dbc18f57de0db3df7acc2d8e8
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-02 12:41:01 +0000

    fix: .snyk & package.json to reduce vulnerabilities

--- libvideogfx/x11/imgwin.cc
+++ libvideogfx/x11/imgwin.cc
@@ -8,4 +8,51 @@
     version 2.1 of the License, or (at your option) any later version.
 
     This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************************************/
+
+#include "config.h"
+
+#include <sys/types.h>
+#include <time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#include <X11/Xlib.h>
+#include <X11/extensions/XShm.h>
+#include <X11/Xutil.h>
+
+#if HAVE_XV
+#include <X11/extensions/Xvlib.h>
+#endif
+
+
+#include "server.hh"
+#include "imgwin.hh"
+
+namespace videogfx {
+
+  struct X11SpecificData
+  {
+    Display*  d_display;
+    Window    d_win;
+  };
+
+
+  ImageWindow_X11::ImageWindow_X11()
+    : d_initialized(false),
+      d_se
